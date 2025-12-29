ce465607c28738bca318d996819dc4e7ff37afd8
sherlockcc50 <sherlockcc50@gmail.com>
2025-12-29 06:30:32 +0200

    #(28) RUN_ASGI NEW

--- backend/run_asgi.py
+++ backend/run_asgi.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "backend.asgi:application",
         host="127.0.0.1",
-        port=8000,
+        port=65534,
         reload=False,
         workers=1,
         log_level="info",
